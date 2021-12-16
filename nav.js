0461bd3414b063fea0348c5325ce8e0948f29246
Eric (Zhenrui) Yu <70619133+yzr1996@users.noreply.github.com>
2021-12-16 12:56:37 -0800

    Delete nav.js

--- nav.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $(".navbar .container-fluid .navbar-nav .dropdown .dropdown-menu").each(function(index) {
-    const num = index + 1;
-    $( this ).addClass("dropdown-menu-" + num);
-  });
-  
-  $(".dropdown-menu-1").append('<li><a href="https://bccandataanalytics.shinyapps.io/IncidenceCounts" target="_blank">BC: New Cancer Dia</a></li>');
-  $(".dropdown-menu-1").append('<li><a href="https://bccandataanalytics.shinyapps.io/IncidenceCrudeRates" target="_blank">BC: Crude Rates</a></li>');
-  
