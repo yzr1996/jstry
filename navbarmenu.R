adc47f81d4df5f9b85bb1e9603173db339da12bc
Eric (Zhenrui) Yu <70619133+yzr1996@users.noreply.github.com>
2021-12-17 09:13:46 -0800

    Delete navbarmenu.R

--- navbarmenu.R
+++
@@ -1,8 +0,0 @@
-navbar_menu <- function() {
-  
-  return(navbarMenu(actionButton("inc", "Incidence Reports")),
-         navbarMenu(actionButton("mort", "Mortality Reports")),
-         navbarMenu(actionButton("prev", "Prevalence Reports")),
-         navbarMenu(actionButton("proj", "Projection Reports")),
-         navbarMenu(actionButton("tab", "Create Your Own Table"))) 
-}
