1462ca339a8bb58ef8c41f6ef6cb456014b5dd7b
Eric (Zhenrui) Yu <70619133+yzr1996@users.noreply.github.com>
2021-12-17 09:14:49 -0800

    Create navbarmenu.R

---
+++ navbarmenu.R
@@ -0,0 +1,5 @@
+navbarMenu(actionButton("inc", "Incidence Reports"))
+navbarMenu(actionButton("mort", "Mortality Reports"))
+navbarMenu(actionButton("prev", "Prevalence Reports"))
+navbarMenu(actionButton("proj", "Projection Reports"))
+navbarMenu(actionButton("tab", "Create Your Own Table"))
